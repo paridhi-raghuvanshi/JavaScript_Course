1d7e160bed7f78b3715d253f348652a0f02baf83
paridhi.raghuvanshi <paridhi.raghuvanshi009@gmail.com>
2026-06-09 20:53:22 +0530

    Strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,25 @@
+const name = "Paridhi"
+const repoCount = 5
+// console.log(name + " have repo: " + repoCount )
+// console.log(`${name.toUpperCase()} have repo: ${repoCount}`)
+const name2 = new String('Tony Stark')
+// console.log(name2[2])
+// console.log(name2.__proto__)
+
+// console.log(name.length)
+// console.log(name2.charAt(3))
+// console.log(name2.indexOf('t'))
+
+const gameName = "GTA 5"
+const anotherGame = gameName.slice(0, 3)
+// console.log(anotherGame)
+
+const myName = "     Paridhi     "
+// console.log(myName)
+// console.log(myName.trim())
+
+const url = "https://www.paridhi.com/paridhi%20chaudhary"
+// console.log(url.replace("%20", "-"))
+// console.log(url.includes("paridhi"))
+
+console.log(gameName.split(" "))
