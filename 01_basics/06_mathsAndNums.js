df1de9f9691e0179cb1429fa7b364874e1830dc0
paridhi.raghuvanshi <paridhi.raghuvanshi009@gmail.com>
2026-06-09 21:20:39 +0530

    nums and math

---
+++ 01_basics/06_mathsAndNums.js
@@ -0,0 +1,38 @@
+const score = 400
+//console.log(score)
+
+const value = new Number(400)
+// console.log(value)
+
+// console.log(score.toString())
+// console.log(score.toString().length)
+// console.log(score.toFixed(4))
+
+const number = 6.928
+// console.log(number.toPrecision(3))
+
+const hundred = 1000000000000
+// console.log(hundred.toExponential())
+// console.log(hundred.toLocaleString())
+// console.log(hundred.toLocaleString('en-IN'))
+
+// ++++++++ MATHS ++++++++
+
+// console.log(Math)
+// console.log(Math.abs(-4))
+// console.log(Math.round(4.3))
+// console.log(Math.ceil(4.3))
+// console.log(Math.floor(4.3))
+// console.log(Math.min(4,3,5,6,7,1,0))
+// console.log(Math.max(4,3,2,5,6,7,1,0))
+// console.log(Math.random())
+
+console.log(Math.random())
+console.log((Math.random()*10)+1)
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random()*(max-min+1)+min))
+
+
+
