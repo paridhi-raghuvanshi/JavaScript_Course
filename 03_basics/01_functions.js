73e15d776aff9b30584558bb289841223557d64b
paridhi.raghuvanshi <paridhi.raghuvanshi009@gmail.com>
2026-06-13 20:43:47 +0530

    Function 2

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -43,4 +43,33 @@ function loginUserMessage(username = "user"){
     }
     return `${username} just logged in`
 }
-console.log(loginUserMessage("Paridhi"))
+// console.log(loginUserMessage("Paridhi"))
+
+function calculateCartPrice(val1, val2, ...num1){
+    return num1   
+}
+// console.log(calculateCartPrice(200,300,400, 70, 800))
+
+const user = {
+    username: "Paridhi",
+    prices: 199
+}
+function handleObject(anyObject){
+    // console.log(`Username is ${anyObject.username} and price is ${anyObject.price}`);
+
+}
+
+handleObject(user)
+handleObject({
+    username: "Pari",
+    price: 399
+})
+
+const myNewArray = [200,400,100,600]
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue([10,40,50,67]));
+
+
