01eb5d4b7e9e58f764b7e21e024dcc145b44e105
paridhi.raghuvanshi <paridhi.raghuvanshi009@gmail.com>
2026-06-15 15:46:03 +0530

    Switch & Truthy, Falsy

---
+++ 04_controlFlow/01_one.js
@@ -0,0 +1,33 @@
+// if
+// if(true){
+
+// }
+// if(false){
+
+// }
+
+// const isUserloggedIn = true
+// if(isUserloggedIn){
+//     return true
+// }
+
+// if(2=="2"){
+//     console.log("True")
+// }
+
+// if(2 === "2"){
+//     console.log("True")
+// } else {
+//     console.log("False")
+// }
+
+const balance = 1000
+if(balance > 500) console.log("test");
+
+if(balance > 500) {
+    console.log("less than")
+} else if (balance < 750){
+    console.log("hb")
+} else {
+    console.log("bb")
+}
---
+++ 04_controlFlow/02_switch.js
@@ -0,0 +1,34 @@
+//  const month = 3
+//  switch (month){
+//     case 1:
+//         console.log("January");
+//         break;
+//     case 2:
+//         console.log("Feb");
+//         break;
+//     case 3:
+//         console.log("March");
+//         break;
+//     case 4:
+//         console.log("April");
+//         break;
+    
+//     default:
+//         break;
+//  }
+
+ // TRUTHY
+ 
+ const userEmail = "P@gmail.com"
+ if(userEmail){
+    console.log("Got");
+ } else {
+    console.log("No gmail");
+ } 
+
+ // Falsy
+//  false, 0, -0, BigInt 0n, "", null, undefined, NaN
+
+// Truhty
+// "0", 'false', " ", [], {}, function(){}
+
